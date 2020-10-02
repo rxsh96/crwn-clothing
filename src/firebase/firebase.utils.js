17582124da41124335812d269fb7555b79291e9d
Ricardo Serrano <rxsh964@gmail.com>
2020-10-01 19:51:30 -0500

    implemented firebase utils including ability to store auth users into firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,31 @@ const config = {
     measurementId: "G-6NZ2W0QTEE"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionaData) => {
+
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({ 
+          displayName,
+          email,
+          createdAt,
+          ...additionaData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    return userRef;
+  }
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SingIn from '../../components/sign-in/sign-in.component'; 
+import SignUp from '../../components/sing-up/sign-up.components';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'><SingIn /></div>
+    <div className='sign-in-and-sign-up'>
+        <SingIn />
+        <SignUp />
+    </div>
 );
 
 export default SignInAndSignUpPage;
